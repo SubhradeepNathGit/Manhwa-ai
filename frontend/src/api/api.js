e249ad8948b7d1051e30a0517d52c6f19de79692
Anurag <anurag20302001@gmail.com>
2025-11-17 15:30:46 +0530

    apipath change

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@
 // API CONFIG
 // ---------------------------------------------------------
 
-const API_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+const API_URL = import.meta.env.VITE_API_BASE_URL;
 
 async function parseJSONResponse(response) {
   try {
